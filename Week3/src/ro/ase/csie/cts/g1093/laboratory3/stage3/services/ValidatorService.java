ebe8344a904de06645f39fe6b0db8ded77fc554a
IoanaGoroneanu <ioana_goroneanu@yahoo.com>
2021-04-08 15:32:44 +0300

    Factory and Facade

---
+++ Week3/src/ro/ase/csie/cts/g1093/laboratory3/stage3/services/ValidatorService.java
@@ -0,0 +1,20 @@
+package ro.ase.csie.cts.g1093.laboratory3.stage3.services;
+
+import ro.ase.csie.cts.g1093.laboratory3.exceptions.InvalidAccountAgeException;
+import ro.ase.csie.cts.g1093.laboratory3.exceptions.InvalidPriceException;
+
+public class ValidatorService implements ValidatorInterface {
+
+	@Override
+	public void validatePrice(float price) throws InvalidPriceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validateAccountAge(int accountAgeInYears) throws InvalidAccountAgeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
